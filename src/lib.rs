3f8eff9cabc24c0128e63e11a595535bba720cc2
Nicolas Riebesel <nicolas.riebesel@gmx.com>
2024-07-08 18:05:10 +0200

    Add subcommands to manage the xwin installation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod common;
+mod init_xwin;
 mod macros;
 mod run;
 mod test;
 
 pub use common::XWinOptions;
+pub use init_xwin::InitXWin;
 pub use macros::{build::Build, check::Check, clippy::Clippy, rustc::Rustc};
 pub use run::Run;
 pub use test::Test;
