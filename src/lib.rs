49246c08a3f89b660ed53e85aa455e3e2f39e066
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2025-06-01 20:58:43 +0300

    Print build envs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod compiler;
+mod env;
 mod macros;
 mod options;
 mod run;
 mod test;
 
+pub use env::Env;
 pub use macros::{build::Build, check::Check, clippy::Clippy, doc::Doc, rustc::Rustc};
 pub use options::XWinOptions;
 pub use run::Run;
